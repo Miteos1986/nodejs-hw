de3bbf29b23ca58554797562d53fa1718e6b0515
smelik81 <artem.smelyanskiy81@gmail.com>
2026-02-18 19:10:50 +0200

    create MongoDb connection

---
+++ src/db/connectMongoDB.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+};
+
+export const connectMongoDB = async () => {
+  try {
+    const uri = process.env.MONGO_URL;
+
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log('✅ MongoDB connection established successfully');
+  } catch (error) {
+    console.error('Failed to connect to db', error);
+    process.exit(1);
+  }
+};
--- src/server.js
+++ src/server.js
@@ -2,19 +2,27 @@ import 'dotenv/config';
 
 import cors from 'cors';
 import express from 'express';
-import pinoHttp from 'pino-http';
 
-const app = express();
-const logger = pinoHttp({
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+//import pinoHttp from 'pino-http';
+
+/*const logger = pinoHttp({
   transport:
-    process.env.NODE_ENV === 'development'
-      ? { target: 'pino-pretty' }
-      : undefined,
-});
-app.use(logger);
+  process.env.NODE_ENV === 'development'
+  ? { target: 'pino-pretty' }
+  : undefined,
+  });*/
+const app = express();
 
-app.use(cors());
+app.use(logger);
 app.use(express.json());
+app.use(cors());
+
+await connectMongoDB();
 
 app.get('/notes', (req, res) => {
   res.json({
@@ -33,20 +41,10 @@ app.get('/notes/:noteId', (req, res, next) => {
   res.json({ message: `Retrieved note with ID:${noteId}` });
 });
 
-//test route for fehler
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
 // undefined route 404
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((error, req, res, next) => {
-  console.log(error);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
+app.use(notFoundHandler);
+// server error 500
+app.use(errorHandler);
 
 const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
 app.listen(PORT, (error) => {
