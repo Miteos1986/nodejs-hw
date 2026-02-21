0a8058835767f4f68324c1f86a812ad6e06d961f
smelik81 <artem.smelyanskiy81@gmail.com>
2026-02-21 13:48:31 +0200

    update logger file

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,6 +2,26 @@ import pino from 'pino-http';
 
 export const logger = pino({
   level: 'info',
+  transport:
+    process.env.NODE_ENV !== 'production'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname',
+            messageFormat:
+              '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+            hideObject: true,
+          },
+        }
+      : undefined,
+});
+
+/*import pino from 'pino-http';
+
+export const logger = pino({
+  level: 'info',
   transport: {
     target: 'pino-pretty',
     options: {
@@ -12,4 +32,4 @@ export const logger = pino({
       hideObject: true,
     },
   },
-});
+});*/
